7b8d87208ca7e907eaf03a6d0b12ac5461b5c33a
rinawata <42815681+rinawata@users.noreply.github.com>
2023-02-15 17:02:39 -0800

    Initial commit

---
+++ app/src/main/java/edu/fullerton/cpsc362/myapplication1/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.fullerton.cpsc362.myapplication1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
